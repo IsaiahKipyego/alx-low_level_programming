3183696c7e7d3a1361c6e32ae662314421f41958
IsaiahKipyegocd <kipyegoisaiah@gmail.com>
2023-08-21 15:35:55 -0700

    result

--- 0x18-dynamic_libraries/ctypes.py
+++ 0x18-dynamic_libraries/ctypes.py
@@ -1,3 +1,11 @@
-#!/usr/bin/python3
+import random
 import ctypes
-spam = ctypes.CDLL(‘./100-operations.so’) 
+
+spam = ctypes.CDLL('https://github.com/IsaiahKipyego/alx-low_level_programming/0x18-dynamic_libraries/100-operations.so')
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
+print("{} + {} = {}".format(a, b, cops.add(a, b)))
+print("{} - {} = {}".format(a, b, cops.sub(a, b)))
+print("{} x {} = {}".format(a, b, cops.mul(a, b)))
+print("{} / {} = {}".format(a, b, cops.div(a, b)))
+print("{} % {} = {}".format(a, b, cops.mod(a, b)))
